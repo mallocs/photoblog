12f01c182da9933a15de675605c76c656d213a6e
Marcus Ulrich <ulrich@mallocs.net>
2022-10-11 13:08:26 -0700

    Fix problem with title on post pages

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -1,5 +1,6 @@
 import { useRouter } from 'next/router'
 import ErrorPage from 'next/error'
+import { TITLE } from '../../lib/constants'
 import Container from '../../components/container'
 import PostBody from '../../components/post-body'
 import Header from '../../components/header'
@@ -35,7 +36,7 @@ export default function Post({ post, morePosts, preview }: Props) {
           <>
             <article className="mb-32">
               <Head>
-                <title>{post.title} | Next.js Photoblog</title>
+                <title>{`${post.title} | ${TITLE}`}</title>
                 <meta property="og:image" content={post.ogImage.url} />
               </Head>
               <PostHeader
