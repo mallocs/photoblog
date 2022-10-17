d24b52695d6719fb0c68b62f8de3944dde2be06f
Marcus Ulrich <ulrich@mallocs.net>
2022-10-16 23:03:08 -0700

    Small css changes

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
       <div className="py-4 flex flex-col lg:flex-row items-center justify-between px-8">
         <div dangerouslySetInnerHTML={{ __html: FOOTER_MESSAGE }} />
         <div className="flex flex-col lg:flex-row justify-center items-center">
-          <a href={SOURCE_URL} className="mx-3 font-bold hover:underline">
+          <a href={SOURCE_URL} className="mx-3">
             View source on GitLab
           </a>
         </div>
