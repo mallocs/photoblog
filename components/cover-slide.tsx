db7ae527419b1a630b6fa7e55c2d0f9d22ad8993
Marcus Ulrich <ulrich@mallocs.net>
2022-12-07 13:27:56 -0800

    Fix: Make sure cover-slide blur data is sized correctly

--- components/cover-slide.tsx
+++ components/cover-slide.tsx
@@ -13,7 +13,7 @@ const CoverSlide = ({ title, slide, slug }: Props) => {
     <NextImage
       // TODO: !bg-auto seems to be necessary atm because nextjs sets the blur image background-size to
       // cover for some reason.
-      className={'!bg-auto object-contain w-full max-h-[100vh]'}
+      className={'!bg-auto !bg-auto object-contain w-full max-h-[100vh]'}
       alt={`Cover image for ${title}`}
       src={slide.url}
       width={Number(slide?.width)}
