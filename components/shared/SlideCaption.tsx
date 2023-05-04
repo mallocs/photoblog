757b06a5f1831a31a789a8f9c1e975ab0d68928b
Marcus Ulrich <ulrich@mallocs.net>
2023-05-04 09:44:24 -0700

    Fix: Make LocationDetails component better able to handle undefined data

--- components/shared/SlideCaption.tsx
+++ components/shared/SlideCaption.tsx
@@ -7,6 +7,16 @@ function LocationDetails({ geodata }) {
   }
 
   const { name, admin1Code, admin2Code, distance } = geodata
+
+  if (admin1Code?.name == undefined && admin2Code?.name == undefined) {
+    return <span>{name}</span>
+  } else if (admin2Code?.name == undefined) {
+    return (
+      <span>
+        {distance < 10 ? `${name}, ${admin1Code.name}` : `${admin1Code.name}`}
+      </span>
+    )
+  }
   return (
     <span>
       {distance < 10
