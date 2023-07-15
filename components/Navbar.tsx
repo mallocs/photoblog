1bb94755b4d2b5032b6309676cd9c6463054c639
Marcus Ulrich <ulrich@mallocs.net>
2023-07-14 17:02:55 -0700

    Feat: Mobile hamburger menu

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,11 @@
 import { useEffect, useState } from 'react'
 import dynamic from 'next/dynamic'
-import { SunIcon, MoonIcon } from '#/components/shared/icons'
+import {
+  SunIcon,
+  MoonIcon,
+  HamburgerOpenIcon,
+  HamburgerCloseIcon,
+} from '#/components/shared/icons'
 import { HomeButton } from '#/components/shared/buttons/HomeButton'
 import { useObserverGroup } from '#/lib/intersection-observer-group'
 import siteConfig from '#/site.config'
@@ -47,7 +52,32 @@ function ColorModeButton() {
   )
 }
 
+function navbarLinks({ block, showText } = { block: false, showText: false }) {
+  {
+    return siteConfig.navbarLinks.map(({ name, iconName, url }: Link) => {
+      return (
+        <a
+          key={name}
+          href={url}
+          className={`${
+            block &&
+            'block border-b border-zinc-700 rounded-none last:border-none'
+          } text-3xl/4 flex items-center gap-3 px-4 py-2 uppercase rounded font-sans font-medium text-zinc-100 dark:text-zinc-100 hover:no-underline hover:text-primary dark:hover:text-primaryDark`}
+        >
+          {iconName !== undefined && (
+            <SIIcons name={`Si${iconName}`} title={name} />
+          )}
+          {(showText || iconName === undefined) && (
+            <span className="text-2xl">{name}</span>
+          )}
+        </a>
+      )
+    })
+  }
+}
+
 export default function Navbar() {
+  const [open, setOpenFn] = useState(false)
   const {
     ref,
     inView: navbarInView,
@@ -61,30 +91,38 @@ export default function Navbar() {
       <nav ref={ref} className="bg-zinc-600 mb-16">
         <div className="mx-auto px-2 sm:px-6 lg:px-8">
           <div className="relative flex h-12 items-center justify-between">
-            <div className="flex flex-1 justify-start items-center space-x-4">
-              {siteConfig.navbarLinks.map(({ name, iconName, url }: Link) => {
-                return (
-                  <a
-                    key={name}
-                    href={url}
-                    className={`${
-                      iconName !== undefined ? 'text-3xl/4' : 'text-2xl'
-                    } px-4 py-2 uppercase rounded font-sans font-medium  text-zinc-100 dark:text-zinc-100 hover:no-underline hover:text-primary dark:hover:text-primaryDark`}
-                  >
-                    {iconName !== undefined ? (
-                      <SIIcons name={`Si${iconName}`} title={name} />
-                    ) : (
-                      name
-                    )}
-                  </a>
-                )
-              })}
+            <div className="absolute inset-y-0 left-0 ml-2 sm:ml-0 flex items-center sm:hidden">
+              {/* Mobile menu button */}
+              <button
+                type="button"
+                className="items-center justify-center w-9 h-9 p-1 rounded-md text-zinc-100 hover:bg-zinc-600 hover:text-primary dark:hover:text-primaryDark focus:outline-none focus:ring-2 focus:ring-inset focus:ring-primary border-zinc-800 border"
+                aria-controls="mobile-menu"
+                aria-expanded="false"
+                onClick={() => setOpenFn(!open)}
+              >
+                <span className="sr-only">Open main menu</span>
+                {open ? <HamburgerOpenIcon /> : <HamburgerCloseIcon />}
+              </button>
+            </div>
+            {/* Full-sized menu */}
+            <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+              <div className="hidden sm:block">
+                <div className="flex space-x-4">{navbarLinks()}</div>
+              </div>
             </div>
             <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
               <ColorModeButton />
             </div>
           </div>
         </div>
+        {/* Mobile menu */}
+        {open && (
+          <div className="sm:hidden" id="mobile-menu">
+            <div className="space-y-1 px-2 pb-2 pt-2 border-t border-zinc-800 shadow-[inset_0_1px_0_rgba(255,255,255,0.1)]">
+              {navbarLinks({ block: true, showText: true })}
+            </div>
+          </div>
+        )}
       </nav>
       {entry !== undefined && (
         <span className="fixed top-4 left-4 z-10">
